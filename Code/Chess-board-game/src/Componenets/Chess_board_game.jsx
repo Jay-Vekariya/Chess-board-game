9fe2ea7bc3e4ed4e5bfe3e2cb684392e1a9a1d18
Jay <jayvekariya2003@gmail.com>
2024-10-17 16:07:42 +0530

    Refactor Chess_board_game component and adjust styling

--- Code/Chess-board-game/src/Componenets/Chess_board_game.jsx
+++ Code/Chess-board-game/src/Componenets/Chess_board_game.jsx
@@ -7,8 +7,8 @@ const Chess_board_game = () => {
 
   return (
     <>
-      <div className=" bg-slate-800  2xl:h-[800px] xl:h-[800px] lg:h-[800px] md:h-[800px] sm:h-[800px] h-[800px]">
-        <h1 className="pt-8 text-white text-center 2xl:text-[50px] xl:text-[50px] lg:text-[45px] md:text-[40px] sm:text-[35px] text-[30px]">
+      <div className="flex flex-col gap-14 bg-slate-800  2xl:h-[800px] xl:h-[800px] lg:h-[800px] md:h-[800px] sm:h-[800px] h-[800px]">
+        <h1 className="pt-8 text-white text-center 2xl:text-[50px] xl:text-[50px] lg:text-[45px] md:text-[40px] sm:text-[35px] text-[45px]">
           Chess board game
         </h1>
         <div className="grid grid-rows-8 grid-cols-8 gap-6 border-2 p-8 m-8 ">
@@ -18,7 +18,7 @@ const Chess_board_game = () => {
               style={{
                 backgroundColor: square.clickedcolor || square.color,
               }}
-              className="2xl:justify-between xl:justify-between  2xl:h-[50px] 2xl:w-[50px] xl:h-[50px] xl:w-[50px] lg:h-[50px] lg:w-[50px] md:h-[50px] md:w-[50px] sm:h-[50px] sm:w-[50px] h-[25px] w-[20px]  cursor-pointer"
+              className="2xl:justify-between xl:justify-between  2xl:h-[50px] 2xl:w-[50px] xl:h-[50px] xl:w-[50px] lg:h-[50px] lg:w-[50px] md:h-[50px] md:w-[50px] sm:h-[50px] sm:w-[50px] h-[23px] w-[20px]  cursor-pointer"
               onClick={() =>
                 dispatch({ type: "CHANGE_COLOR", payload: { index } })
               }
