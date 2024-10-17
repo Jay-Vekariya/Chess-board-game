dcee7ab6281a1c369d0cc7e8841ab113cbac6d11
Jay <jayvekariya2003@gmail.com>
2024-10-17 16:12:44 +0530

    Refactor Chess_board_game component styling and dimensions

--- Code/Chess-board-game/src/Componenets/Chess_board_game.jsx
+++ Code/Chess-board-game/src/Componenets/Chess_board_game.jsx
@@ -7,8 +7,8 @@ const Chess_board_game = () => {
 
   return (
     <>
-      <div className="flex flex-col gap-14 bg-slate-800  2xl:h-[800px] xl:h-[800px] lg:h-[800px] md:h-[800px] sm:h-[800px] h-[800px]">
-        <h1 className="pt-8 text-white text-center 2xl:text-[50px] xl:text-[50px] lg:text-[45px] md:text-[40px] sm:text-[35px] text-[45px]">
+      <div className="flex flex-col gap-10 bg-slate-800  2xl:h-[900px] xl:h-[1000px] lg:h-[900px] md:h-[900px] sm:h-[900px] h-[800px]">
+        <h1 className="uppercase pt-8 text-white text-center 2xl:text-[50px] xl:text-[50px] lg:text-[45px] md:text-[40px] sm:text-[35px] text-[45px]">
           Chess board game
         </h1>
         <div className="grid grid-rows-8 grid-cols-8 gap-6 border-2 p-8 m-8 ">
