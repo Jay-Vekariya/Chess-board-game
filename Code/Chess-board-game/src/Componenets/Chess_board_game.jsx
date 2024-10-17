3f06cf6f080d5babccd630f1e132aca7e2ae6a00
Jay <jayvekariya2003@gmail.com>
2024-10-17 15:26:42 +0530

    Update package dependencies and add Chess_board_game component

--- Code/Chess-board-game/src/Componenets/Chess_board_game.jsx
+++ Code/Chess-board-game/src/Componenets/Chess_board_game.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 const Chess_board_game = () => {
-  return <div>Chess_board_game</div>;
+  const squares = useSelector((state) => state.squares);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div className=" bg-slate-800 h-screen">
+        <h1 className="pt-8 text-white text-center 2xl:text-[50px] xl:text-[50px] lg:text-[45px] md:text-[40px] sm:text-[35px] text-[30px]">
+          Chess board game
+        </h1>
+        <div className=" flex-wrap grid grid-rows-8 grid-cols-8 gap-6 border-2 p-8 m-8">
+          {squares.map((square, index) => (
+            <div
+              key={index}
+              style={{
+                backgroundColor: square.clickedcolor || square.color,
+              }}
+              className="2xl:justify-between xl:justify-between  2xl:h-[50px] 2xl:w-[50px] xl:h-[50px] xl:w-[50px] lg:h-[50px] lg:w-[50px] md:h-[50px] md:w-[50px] sm:h-[50px] sm:w-[50px] h-[25px] w-[20px]  cursor-pointer"
+              onClick={() =>
+                dispatch({ type: "CHANGE_COLOR", payload: { index } })
+              }
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Chess_board_game;
--- Code/Chess-board-game/src/Componenets/Redux_store.jsx
+++ Code/Chess-board-game/src/Componenets/Redux_store.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import { createStore } from "redux";
 
-const initialState = {};
+const initialState = {
+  squares: Array(64)
+    .fill()
+    .map((_, index) => ({
+      color: Math.floor(index / 8) % 2 === 0 ? "white" : "black",
+      clickedcolor: null,
+    })),
+};
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case "CHANGE_COLOR":
+      const updatedSquares = state.squares.map((square, index) => {
+        if (index === action.payload.index) {
+          return {
+            ...square,
+            clickedcolor: square.color === "white" ? "yellow" : "red",
+          };
+        }
+        return square;
+      });
+      return {
+        ...state,
+        squares: updatedSquares,
+      };
+    case "CLICKED_SQUARE":
+      return {
+        ...state,
+        clickedSquare: action.payload,
+      };
     default:
       return state;
   }
